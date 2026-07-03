33d52f50803e012e77a51a3d24e2d7d989a90113
Vladyslav Davydenko <vladyslav@hey.com>
2026-07-03 19:24:57 +0300

    Allow * in domain name

--- app/models/service.rb
+++ app/models/service.rb
@@ -12,7 +12,7 @@ class Service < ApplicationRecord
   validates :name,          presence: true
   validates :hostname,      presence: true,
                             uniqueness: { case_sensitive: false },
-                            format: { with: /\A[a-z0-9.-]+\z/i, message: "must be a valid hostname" }
+                            format: { with: /\A[a-z0-9.*-]+\z/i, message: "must be a valid hostname" }
   validates :upstream_host, presence: true
   validates :upstream_port, presence: true,
                             numericality: { only_integer: true, in: 1..65535 }
